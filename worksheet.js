02b4224938cc9b70e802ca6f58c44707484a4775
Jay Lee <dreampoetlee@gmail.com>
2022-04-27 05:58:08 -0400

    Added an if/else conditional statement to the worksheet.

--- worksheet.js
+++ worksheet.js
@@ -20,11 +20,24 @@ let favBreak = 'chicken biscuit w/cheese';
 let favLunch = 'Asiago Spicy Chicken combo';
 let favDinner = 'Pizza';
 let timeOfDay = 1200;
-if (timeOfDay < 1200) {
+if (timeOfDay <= 1200) {
   foodChoice = favBreak;
 } else if (timeOfDay > 1200 && timeOfDay < 1700) {
   foodChoice = favLunch;
-} else if (timeOfDay > 1700) {
+} else if (timeOfDay >= 1700) {
   foodChoice = favDinner
 }
 console.log(foodChoice);
+
+// 2) Random Number
+let myNum = Math.floor(Math.random() * 11);
+if (myNum <=0 && myNum >= 2) {
+  console.log('Beatles');
+} else if (myNum <= 3 && myNum >= 5) {
+  console.log('Stones');
+} else if (myNum <= 6 && myNum >= 8) {
+  console.log('Floyd');
+} else if (myNum == 9 || myNum == 10) {
+  console.log('Hendrix');
+}
+console.log(myNum)
